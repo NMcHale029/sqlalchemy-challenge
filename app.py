372f11e3ef1918bcc8b7a9139cbe73ce276c682a
NMcHale029 <noah.reetz@gmail.com>
2023-09-13 11:51:18 -0500

    debugged app

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite",
+                       connect_args={'check_same_thread': False})
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -34,6 +35,8 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+
+
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -41,9 +44,10 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start_end"
     )
+
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -58,9 +62,9 @@ def precipitation():
     last_12_months = session.query(measurement.date, measurement.prcp)\
         .filter(measurement.date >= one_year_ago)\
         .order_by(measurement.date.desc()).all()
-    
+
     session.close()
-    
+
     # Set an empty dictionary to put data into
     prcp_dict = {}
 
@@ -71,11 +75,16 @@ def precipitation():
     # Return a JSON list of stations from the dataset
     return jsonify(prcp_dict)
 
+
 @app.route("/api/v1.0/stations")
 def stations():
-    
+
+    station_info = session.query(station.station).all()
+    station_info = list(np.ravel(station_info))
+
     # Return a JSON lis of stations from the dataset
-    return jsonify(station)
+    return jsonify(station_info)
+
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -91,62 +100,66 @@ def tobs():
 
     session.close()
 
+    temps = list(np.ravel(temps))
+
     # Return a JSON list of temperature observations for the previous year
-    return temps
+    return jsonify(temps=temps)
+
 
 @app.route("/api/v1.0/<start>")
-def start():
+def begining(start=None):
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     # Query temperature observations from a given start date to the end of the data set
     print('Please indicate from which date you would like temperature data:')
-    year = input('From what year? ')
-    month = input('From what month? ')
-    day = input('From what day? ')
-    
-    query_date = dt.date(year, month, day)
-    
-    temp_data = engine.execute(text(f'SELECT MIN(tobs), MAX(tobs), AVG(tobs)\
-    FROM measurement\
-    WHERE date >= {query_date}')).all()
-    
+    # year = input('From what year? ')
+    # month = input('From what month? ')
+    # day = input('From what day? ')
+
+    query_date = dt.datetime.strptime(start, "%m%d%Y")
+
+    # temp_data = engine.execute(text(f'SELECT MIN(tobs), MAX(tobs), AVG(tobs)\
+    # FROM measurement\
+    # WHERE date >= {query_date}')).all()
+    select = [func.min(measurement.tobs), func.max(
+        measurement.tobs), func.avg(measurement.tobs)]
+    temp_data = session.query(
+        *select).filter(measurement.date >= query_date).all()
+
     session.close()
 
-    # Returns the min, max, and average temperatures calculated from 
+    # Returns the min, max, and average temperatures calculated from
     # the given start date to the end of the dataset
-    return temp_data
+    temp_data = list(np.ravel(temp_data))
+
+    return jsonify(temp_data=temp_data)
+
 
 @app.route("/api/v1.0/<start>/<end>")
-def start_end():
+def start_end(start=None, end=None):
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     # Accepts the start and end dates as parameters
-    print('Please the start date you would like temperature data:')
-    start_year = input('From what year? ')
-    start_month = input('From what month? ')
-    start_day = input('From what day? ')
 
-    start_date = dt.date(start_year, start_month, start_day)
+    start_date = dt.datetime.strptime(start, "%m%d%Y")
+    end_date = dt.datetime.strptime(end, "%m%d%Y")
 
-    print('Please the end date you would like temperature data:')
-    end_year = input('From what year? ')
-    end_month = input('From what month? ')
-    end_day = input('From what day? ')   
-    
-    end_date = dt.date(end_year, end_month, end_day)
+    # Queries the min, max, and average of observed temperature in the selected timeframe.
+    select = [func.min(measurement.tobs), func.max(
+        measurement.tobs), func.avg(measurement.tobs)]
+    temp_data = session.query(*select).filter(measurement.date >= start_date)\
+        .filter(measurement.date <= end_date).all()
 
-    # Queries the min, max, and average of observed temperature in the selected timeframe.    
-    start_end_data = engine.execute(text(f'SELECT MIN(tobs), MAX(tobs), AVG(tobs)\
-    FROM measurement\
-    WHERE date >= {start_date} AND date <= {end_date}')).all()
-    
     session.close()
 
-    # Returns the min, max, and average temperatures calculated from 
+    # Returns the min, max, and average temperatures calculated from
     # the given start date to the end of the dataset
-    return start_end_data
+    temp_data = list(np.ravel(temp_data))
+
+    return jsonify(temp_data=temp_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
